75f130384ece57a55c5273eb98953adabe83e7d6
Anna Urbala <anna.urbala@getindata.com>
2023-01-05 10:56:07 +0100

    demo example

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,28 @@
-FROM registry.ververica.com/v2.9/flink:1.15.3-stream1-scala_2.12-java8
+FROM flink:1.16-java8
 
 USER root
 
-RUN pip install pipenv
+RUN set -ex && \
+    apt update && \
+    apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev \
+    libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev && \
+    wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz && \
+    tar -xf Python-3.8.12.tgz && \
+    rm -rf Python-3.8.12.tgz && \
+    cd Python-3.8.12 && \
+    ./configure && \
+    make -j 8 && \
+    make altinstall && \
+    ln -s /usr/local/bin/python3.8 /usr/bin/python && \
+    ln -s /usr/local/bin/pip3.8 /usr/bin/pip && \
+    pip install --upgrade pip && \
+    pip install --upgrade setuptools && \
+    pip install pipenv && \
+    apt-get clean
+
+RUN mkdir -p /flink/opt/ && \
+    chown -R flink:flink /flink/opt/ && \
+    chown -R flink:flink /opt/flink
 
 USER flink
 
@@ -10,8 +30,7 @@ COPY ./Pipfile ./Pipfile.lock /flink/opt/
 
 RUN set -ex && \
     cd /flink/opt && \
-    pipenv requirements > requirements.txt && \
-    pip install -r requirements.txt
+    pipenv install --system
 
 COPY ./src /app/src
 COPY .streaming_config.yml ./jars/* /app/lib/
--- src/flink_app.py
+++ src/flink_app.py
@@ -1,21 +1,75 @@
+from pyflink.common import Row
 from pyflink.datastream.stream_execution_environment import StreamExecutionEnvironment
-from pyflink.table import DataTypes, CsvTableSink, StreamTableEnvironment
+from pyflink.table import DataTypes, StreamTableEnvironment, TableDescriptor, Schema, TableEnvironment, \
+    EnvironmentSettings
+from pyflink.table.expressions import col, lit
+from pyflink.table.udf import udtf
+
+word_count_data = ["To be, or not to be,--that is the question:--",
+                   "Whether 'tis nobler in the mind to suffer",
+                   "The slings and arrows of outrageous fortune",
+                   "Or to take arms against a sea of troubles,",
+                   "And by opposing end them?--To die,--to sleep,--",
+                   "No more; and by a sleep to say we end",
+                   "The heartache, and the thousand natural shocks",
+                   "That flesh is heir to,--'tis a consummation",
+                   "Devoutly to be wish'd. To die,--to sleep;--",
+                   "To sleep! perchance to dream:--ay, there's the rub;",
+                   "For in that sleep of death what dreams may come,",
+                   "When we have shuffled off this mortal coil,",
+                   "Must give us pause: there's the respect",
+                   "That makes calamity of so long life;",
+                   "For who would bear the whips and scorns of time,",
+                   "The oppressor's wrong, the proud man's contumely,",
+                   "The pangs of despis'd love, the law's delay,",
+                   "The insolence of office, and the spurns",
+                   "That patient merit of the unworthy takes,",
+                   "When he himself might his quietus make",
+                   "With a bare bodkin? who would these fardels bear,",
+                   "To grunt and sweat under a weary life,",
+                   "But that the dread of something after death,--",
+                   "The undiscover'd country, from whose bourn",
+                   "No traveller returns,--puzzles the will,",
+                   "And makes us rather bear those ills we have",
+                   "Than fly to others that we know not of?",
+                   "Thus conscience does make cowards of us all;",
+                   "And thus the native hue of resolution",
+                   "Is sicklied o'er with the pale cast of thought;",
+                   "And enterprises of great pith and moment,",
+                   "With this regard, their currents turn awry,",
+                   "And lose the name of action.--Soft you now!",
+                   "The fair Ophelia!--Nymph, in thy orisons",
+                   "Be all my sins remember'd."]
+
+
+@udtf(result_types=[DataTypes.STRING()])
+def split(line: Row):
+    for s in line[0].split():
+        yield Row(s)
 
 
 def tutorial():
-    env = StreamExecutionEnvironment.get_execution_environment()
-    env.set_parallelism(1)
+    # env = StreamExecutionEnvironment.get_execution_environment()
 
-    field_names = ['a', 'b', 'c']
-    field_types = [DataTypes.BIGINT(), DataTypes.STRING(), DataTypes.STRING()]
+    # t_env = StreamTableEnvironment.create(env)
+    t_env = TableEnvironment.create(EnvironmentSettings.in_streaming_mode())
+    tab = t_env.from_elements(map(lambda i: (i,), word_count_data),
+                              DataTypes.ROW([DataTypes.FIELD('line', DataTypes.STRING())]))
 
-    t_env = StreamTableEnvironment.create(env)
-    t_env.register_table_sink(
-        'Results',
-        CsvTableSink(field_names, field_types, '/tmp/test.csv'))
-    t_env.insert_into('Results', t_env.from_elements([(1, 'Hi', 'Hello')], ['a', 'b', 'c']))
+    t_env.create_temporary_table(
+        'sink',
+        TableDescriptor.for_connector('print')
+        .schema(Schema.new_builder()
+                .column('word', DataTypes.STRING())
+                .column('count', DataTypes.BIGINT())
+                .build())
+        .build())
 
-    t_env.execute('test_stream_execute')
+    tab.flat_map(split).alias('word') \
+        .group_by(col('word')) \
+        .select(col('word'), lit(1).count) \
+        .execute_insert('sink') \
+        .wait()
 
 
 if __name__ == '__main__':
